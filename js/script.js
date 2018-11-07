45b957e47a3e12b64d767c7497e3565a9afdab72
llavenovemiel <llavenovemiel@gmail.com>
2018-11-07 22:03:44 +0800

    wraps in an IIFE

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
-const result = document.querySelector("h1");
+(function(){
+	const result = document.querySelector("h1");
 
-const operators = document.querySelectorAll(".operators");
+	const operators = document.querySelectorAll(".operators");
 
-const operationList = {add:(a,b) => (a + b),
-					subtract:(a,b) => (a - b),
-					multiply:(a,b) => (a * b),
-					divide:(a,b) => (a / b)}
+	const operationList =	{add:(a,b) => (a + b),
+							subtract:(a,b) => (a - b),
+							multiply:(a,b) => (a * b),
+							divide:(a,b) => (a / b)}
 
-operators.forEach(operator => operator.addEventListener("click", () => {
-	let input1 = document.querySelector("#input1").value * 1;
-	let input2 = document.querySelector("#input2").value * 1;
-	let operation = operationList[operator.innerHTML.toLowerCase()];
-	result.innerHTML = operation(input1, input2);	
-}))
+	let input1, input2, operation;
+
+	operators.forEach(operator => operator.addEventListener("click", () => {
+		input1 = document.querySelector("#input1").value * 1;
+		input2 = document.querySelector("#input2").value * 1;
+		operation = operationList[operator.innerHTML.toLowerCase()];
+		result.innerHTML = operation(input1, input2);	
+	}))
+
+
+	// //below is the for loop version of above
+
+	// for (let operator of operators) {
+	// 	operator.addEventListener("click", function () {
+	// 		input1 = document.querySelector("#input1").value * 1;
+	// 		input2 = document.querySelector("#input2").value * 1;
+	// 		operation = operationList[operator.innerHTML.toLowerCase()];
+	// 		result.innerHTML = operation(input1, input2);
+	// 	})
+	// }
+
+})();
+
+
